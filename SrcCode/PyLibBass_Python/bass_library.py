d953ef46984a852c3837e622e533b4c4fe4ebfe5
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-03-17 17:04:10 +0200

    Fixed many errors
    Fixed many errors!!! You can now Using My Library!!! :D

--- SrcCode/PyLibBass_Python/bass_library.py
+++ SrcCode/PyLibBass_Python/bass_library.py
@@ -8,15 +8,19 @@ def ConnectBASS():
         except:
             raise RuntimeError("[BASS]: Not Founded BASS.dll or Him Dependencies!!!")
 class BASS:
-    def BASS_INIT(self, device : int, freq : ctypes.c_int32, flags : ctypes.c_int32, win : int, dsguid : int):
-        return ConnectBASS().BASS_Init(self, device, freq, flags, win, dsguid)
+    def BASS_INIT(self, device : ctypes.c_int, freq : ctypes.c_int32, flags : ctypes.c_int32, win : ctypes.c_int, dsguid : ctypes.c_int) -> ctypes.c_bool:
+        ConnectBASS().BASS_Init.restype = ctypes.c_bool
+        ConnectBASS().BASS_Init.argtypes = [ctypes.c_int, ctypes.c_int32, ctypes.c_int32, ctypes.c_int, ctypes.c_int]
+        return ConnectBASS().BASS_Init(device, freq, flags, win, dsguid)
     def BASSFree(self):
         return ConnectBASS().BASS_Free()
     def BASSStop(self):
         return ConnectBASS.BASS_Stop()
-    def BASS_StreamCreateFile(self, mem : int, filename : str, offset : int, length : int, flags : int):
+    def BASS_StreamCreateFile(self, mem : ctypes.c_int, filename : ctypes.c_char, offset : ctypes.c_int, length : ctypes.c_int, flags : ctypes.c_int) -> ctypes.c_ulong:
+        ConnectBASS().BASS_StreamCreateFile.restype = ctypes.c_ulong
+        ConnectBASS().BASS_StreamCreateFile.argtypes = [ctypes.c_int, ctypes.c_char, ctypes.c_int, ctypes.c_int, ctypes.c_int]
         return ConnectBASS().BASS_StreamCreateFile(mem, filename, offset, length, flags)
     def BASS_START(self):
         return ConnectBASS().BASS_Start()
-    def BASS_ChannelPlay(self, handle, restart : bool):
+    def BASS_ChannelPlay(self, handle : int, restart : bool):
         return ConnectBASS().BASS_ChannelPlay(handle, restart)
