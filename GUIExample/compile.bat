f32e0f670d4ecf8c0f0820ecef76c644c11de805
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-03-31 18:20:33 +0300

    Update compile.bat

--- GUIExample/compile.bat
+++ GUIExample/compile.bat
@@ -1,3 +1,3 @@
 @echo off
 
-pyinstaller Main.py --windowed --onefile --name "PyLibBASS_Example" --icon "pylibbass.ico" --collect-binaries "bass" --collect-all "PyLibBass_Python"
+pyinstaller Main.py --windowed --onefile --name "PyLibBASS_Example" --icon "pylibbass.ico" --collect-all "PyLibBass_Python"
