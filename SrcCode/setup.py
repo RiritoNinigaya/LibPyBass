0c4077d9f338d3a2934a7f7647720417f6fb86d3
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-04-14 00:36:39 +0300

    Update setup.py

--- SrcCode/setup.py
+++ SrcCode/setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     author_email='riritoninigaya@hotmail.com',
     author='Ririto Ninigaya',
-    license='MIT License,
+    license='MIT License',
     url='https://github.com/RiritoNinigaya/LibPyBass',
     description='My First Library For BASS(WORKS ONLY X64!!!)',
     packages=setuptools.find_packages(),
